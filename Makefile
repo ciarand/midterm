efd71de0fdb8d13fec73c7fbf04d0fedc68e4c72
Ciaran Downey <me@ciarand.me>
2014-03-09 22:53:51 -0700

    Add an app.php test runner and a Makefile for it

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	php app.php
---
+++ app.php
@@ -0,0 +1,17 @@
+<?php require "vendor/autoload.php";
+
+use Ciarand\Midterm\GlobalState;
+use Ciarand\Midterm\Reporter\PlainTextReporter;
+use Ciarand\Midterm\Reporter\DotReporter;
+use Ciarand\Midterm\Reporter\TapReporter;
+
+include "spec/Reporter/DotReporterSpec.php";
+include "spec/Reporter/TapReporterSpec.php";
+include "spec/SuiteHelperSpec.php";
+include "spec/SpecSpec.php";
+include "spec/Expectation/StrictEqualityMatcherSpec.php";
+
+$midterm = GlobalState::getMidterm();
+//$midterm->addReporter(new TapReporter);
+$midterm->addReporter(new DotReporter);
+$midterm->go();
