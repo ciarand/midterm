7ed9b78b9c758f1a4941695994a4fd0aedf25c32
Ciaran Downey <me@ciarand.me>
2014-03-16 22:17:17 -0700

    Rename the DotReporter specs to make lines shorter

--- spec/Reporter/DotReporterSpec.php
+++ spec/Reporter/DotReporterSpec.php
@@ -15,27 +15,21 @@ describe("DotReporter", function ($vars) {
     $failure->getMessage()->willReturn("foo was not bar");
     $failure->title = "test title";
 
-    it(
-        "prints an F for a failing test",
-        function () use ($failure) {
-            $reporter = new DotReporter;
+    it("prints an F for a failing test", function () use ($failure) {
+        $reporter = new DotReporter;
 
-            expect(function () use ($reporter, $failure) {
-                $reporter->onSpecFail($failure->reveal());
-            })->when()->run()->toOutput("F");
-        }
-    );
+        expect(function () use ($reporter, $failure) {
+            $reporter->onSpecFail($failure->reveal());
+        })->when()->run()->toOutput("F");
+    });
 
-    it(
-        "prints a . for a passing test",
-        function () use ($success) {
-            $reporter = new DotReporter;
+    it("prints a . for a passing test", function () use ($success) {
+        $reporter = new DotReporter;
 
-            expect(function () use ($reporter, $success) {
-                $reporter->onSpecPass($success->reveal());
-            })->when()->run()->toOutput(".");
-        }
-    );
+        expect(function () use ($reporter, $success) {
+            $reporter->onSpecPass($success->reveal());
+        })->when()->run()->toOutput(".");
+    });
 
     it("prints the time taken in a test run", function () {
         $callback = callback(function () {
@@ -58,78 +52,63 @@ describe("DotReporter", function ($vars) {
             ->output()->match('/Memory: ([0-9]+\.[0-9]+)*/m');
     });
 
-    it(
-        "prints OK when no tests failed",
-        function () use ($success) {
-            $callback = callback(function () use ($success) {
-                $reporter = new DotReporter;
-
-                for ($i = 0; $i < 3; $i += 1) {
-                    $reporter->onUpdate($success->reveal());
-                }
-                $reporter->onTestEnd();
-            });
-
-            expect($callback)->when()->run()->output()->match("/^OK/m");
-        }
-    );
-
-    it(
-        "prints FAILURES! when at least one test failed",
-        function () use ($failure) {
-            $callback = callback(function () use ($failure) {
-                $reporter = new DotReporter;
-                $reporter->onUpdate($failure->reveal());
-
-                $reporter->onTestEnd();
-            });
-
-            expect($callback)->when()->run()->output()->match("/^FAILURES!/m");
-        }
-    );
-
-    it(
-        "prints the number of tests with no failures",
-        function () use ($success) {
-            $callback = callback(function () use ($success) {
-                $reporter = new DotReporter;
-
-                for ($i = 0; $i < 3; $i += 1) {
-                    $reporter->onUpdate($success->reveal());
-                }
-
-                $reporter->onTestEnd();
-            });
-
-            expect($callback)->when()->run()->output()->match("/\(3 tests\)/");
-        }
-    );
-
-    it(
-        "prints the number of tests with some failures",
-        function () use ($failure, $success) {
+    it("prints OK when no tests failed", function () use ($success) {
+        $callback = callback(function () use ($success) {
             $reporter = new DotReporter;
 
+            for ($i = 0; $i < 3; $i += 1) {
+                $reporter->onUpdate($success->reveal());
+            }
+            $reporter->onTestEnd();
+        });
+
+        expect($callback)->when()->run()->output()->match("/^OK/m");
+    });
+
+    it("prints FAILURES! when a test fails", function () use ($failure) {
+        $callback = callback(function () use ($failure) {
+            $reporter = new DotReporter;
             $reporter->onUpdate($failure->reveal());
+
+            $reporter->onTestEnd();
+        });
+
+        expect($callback)->when()->run()->output()->match("/^FAILURES!/m");
+    });
+
+    it("prints the number of tests with 0 fails", function () use ($success) {
+        $callback = callback(function () use ($success) {
+            $reporter = new DotReporter;
 
             for ($i = 0; $i < 3; $i += 1) {
                 $reporter->onUpdate($success->reveal());
             }
 
-            expect(array($reporter, "onTestEnd"))->when()->run()
-                ->output()->match("/Failures: 1/");
+            $reporter->onTestEnd();
+        });
+
+        expect($callback)->when()->run()->output()->match("/\(3 tests\)/");
+    });
+
+    it("prints # of tests w/ >0 fails", function () use ($failure, $success) {
+        $reporter = new DotReporter;
+
+        $reporter->onUpdate($failure->reveal());
+
+        for ($i = 0; $i < 3; $i += 1) {
+            $reporter->onUpdate($success->reveal());
         }
-    );
 
-    it(
-        "prints the failed spec names",
-        function () use ($failure) {
-            $reporter = new DotReporter;
+        expect(array($reporter, "onTestEnd"))->when()->run()
+            ->output()->match("/Failures: 1/");
+    });
 
-            $reporter->onUpdate($failure->reveal());
+    it("prints the failed spec names", function () use ($failure) {
+        $reporter = new DotReporter;
 
-            expect(array($reporter, "onTestEnd"))->when()->run()
-                ->output()->match("/^1\) test title: foo was not bar$/m");
-        }
-    );
+        $reporter->onUpdate($failure->reveal());
+
+        expect(array($reporter, "onTestEnd"))->when()->run()
+            ->output()->match("/^1\) test title: foo was not bar$/m");
+    });
 });
